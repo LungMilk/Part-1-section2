c66549bbce263a843bf71a90697290f3bff64b67
DESKTOP-NJ4NBMM\murra <murrayjenkins9001@gmail.com>
2024-01-30 19:35:16 -0500

    attempting to rework the ability for the spawn block to spawn in ten seperate locations using loops at once

--- Assets/Assignment/Scripts/Spawner.cs
+++ Assets/Assignment/Scripts/Spawner.cs
@@ -1,15 +1,25 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    
     public GameObject pin;
-    public Transform spawnPoint;
+    List<GameObject> listOfSpawn = new List<GameObject>(10);
+    public GameObject spawnPoint3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < listOfSpawn.Count; i++)
+        {
+            //this declares the new gameObject with the name spawnPointi so it would incriment with the for loop making spawnPoint1 then spawnPoint2
+            //it only makes new game objects not taking existing ones
+            GameObject spawnPoint = new GameObject("SpawnPoint" + i);
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +30,7 @@ public class Spawner : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Instantiate(pin, spawnPoint);
+        Instantiate(pin, listOfSpawn[0].transform);
         Debug.Log("Pin Spawned");
         
     }
