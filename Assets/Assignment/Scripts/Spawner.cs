e389b9b70879332cdb946190a05947753f8fac7c
DESKTOP-NJ4NBMM\murra <murrayjenkins9001@gmail.com>
2024-01-30 19:48:38 -0500

    changed how the for looped worked now will find all transforms and have those be spawn points for pins

--- Assets/Assignment/Scripts/Spawner.cs
+++ Assets/Assignment/Scripts/Spawner.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject pin;
+<<<<<<< Updated upstream
     public Transform spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
         
+=======
+    Transform[] spawnPoints = FindObjectsOfType<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -20,8 +28,16 @@ public class Spawner : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+<<<<<<< Updated upstream
         Instantiate(pin, spawnPoint);
         Debug.Log("Pin Spawned");
+=======
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(pin, spawnPoints[i]);
+            Debug.Log("Pin Spawned");
+        }
+>>>>>>> Stashed changes
         
     }
 }
